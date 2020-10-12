fc04c0ca10bac116f5370b9dfa6cc498ba63c00a
Yassine <yassin23raki@gmail.com>
2020-10-12 21:42:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+fs.readdir(`${process.cwd()}`, (err, filenames) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(filenames);
+})
