959973e2a806156472c68c406a185286c3a280cc
Yassine <yassin23raki@gmail.com>
2020-10-13 09:37:28 +0100

    v3.1 ( colors & path control )

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const chalk = require('chalk')
+const path = require('path');
 
 const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, filenames) => {
     if (err) {
         console.log(err);
     }
 
     const statPromises = filenames.map( (filename) => {
-        return lstat(filename);
+        return lstat(path.join(targetDir) ,filename);
     });
 
     const allStats = await Promise.all(statPromises);
 
     for (let stats of allStats) {
         const index = allStats.indexOf(stats);
-        
-        console.log(filenames[index], stats.isFile());
+
+        if (stats.isFile()) {
+            console.log(filenames[index]);
+        } else {
+            console.log(chalk.yellow(filenames[index]))
+        }
     }
 })
 
